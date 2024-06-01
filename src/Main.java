cb01fcdd7d0146a85d4dcb634ed3254098b0e85f
Pr1nkos <prinkos1@gmail.com>
2024-06-01 09:20:42 +0300

    Добавил отображение самой длинной последовательности

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,63 @@
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
-        String word1 = "gorbusha";
-        String word2 = "solusha";
+        String word1 = "fish";
+        String word2 = "fosh";
         SearchLCS(word1, word2);
-
     }
 
-    static void SearchLCS (String word1, String word2) {
-        int [][] cells = new int[word1.length() + 1][word2.length() + 1];
+    static void SearchLCS(String word1, String word2) {
+        int[][] cells = new int[word1.length() + 1][word2.length() + 1];
 
-        //Вычисление длины наибольшей последовательности
+        // Вычисление длины наибольшей последовательности
         for (int i = 1; i <= word1.length(); i++) {
             for (int j = 1; j <= word2.length(); j++) {
                 if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    cells[i][j] = cells[i-1][j-1] + 1;
+                    cells[i][j] = cells[i - 1][j - 1] + 1;
                 } else {
-                    cells[i][j] = Math.max(cells[i-1][j], cells[i][j-1]);
+                    cells[i][j] = Math.max(cells[i - 1][j], cells[i][j - 1]);
                 }
             }
         }
 
-        //Создание таблицы
-        System.out.print(" ");
+        // Создание таблицы
+        System.out.print("  ");
         for (int j = 0; j < word2.length(); j++) {
             System.out.print(" " + word2.charAt(j));
         }
         System.out.println();
 
-        for (int i = 1; i <= word1.length(); i++) {
+        for (int i = 0; i <= word1.length(); i++) {
             if (i > 0) {
                 System.out.print(word1.charAt(i - 1) + " ");
             } else {
                 System.out.print("  ");
             }
-            for (int j = 1; j <= word2.length(); j++) {
+            for (int j = 0; j <= word2.length(); j++) {
                 System.out.print(cells[i][j] + " ");
             }
             System.out.println();
         }
-        //Результат
+
+        // Результат
         System.out.println();
         System.out.println("Длина наибольшей общей подпоследовательности: " + cells[word1.length()][word2.length()]);
+
+        // Восстановление наибольшей общей подпоследовательности
+        StringBuilder lcs = new StringBuilder();
+        int i = word1.length(), j = word2.length();
+        while (i > 0 && j > 0) {
+            if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                lcs.append(word1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (cells[i - 1][j] > cells[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        // Печать LCS
+        System.out.println("Наибольшая общая подпоследовательность: " + lcs.reverse().toString());
     }
 }
